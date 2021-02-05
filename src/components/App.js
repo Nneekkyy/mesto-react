5141b72b32d8d282f8d8cd159d789254e3d73c2d
Alexey Krasnov <neky@list.ru>
2021-02-05 13:29:38 +0500

    some function(trash button) + refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,20 @@ import PopupWithForm from './PopupWithForm'
 import ImagePopup from './ImagePopup'
 import ESC_KEYCODE from '../utils/keycode'
 import api from "../utils/Api";
-
+import {UserContext} from '../contexts/CurrentUserContext'
 
 function App() {
-    const [currentUser, setCurrentUser] = useState('');
-
+    const [currentUser, setCurrentUser] = useState({
+        name: '',
+        about: '',
+        avatar: ''
+    });
+    const [cards, setCards] = useState([]);
     useEffect(() => {
         api.getAllData()
             .then((response => {
-                const [userData] = response;
+                const [userData, cardsData] = response;
+                setCards(cardsData);
                 setCurrentUser(userData);
             }))
             .catch((err) => {
@@ -59,50 +64,53 @@ function App() {
         }
     }
     return (
+        <UserContext.Provider value={currentUser}>
+            <div className="body" onKeyDown={onKeyPressed} tabIndex="0" >
+                <div className="page" >
+                    <Header/>
+                    <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick}
+                          onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick}
+                          cards={cards} />
 
-        <div tabIndex="0" className="body" onKeyDown={onKeyPressed}>
-            <div className="page" >
-                <Header/>
-                <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick}
-                      onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick} />
+                    <Footer/>
 
-                <Footer/>
-
-                <PopupWithForm id='popupEdit' title='Редактировать профиль' name='edit-profile'
-                               isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
-                    <input id="nameField" className="popup__field popup__field_name" type="text" name="name"
-                           placeholder="Имя"
-                           minLength="2" maxLength="40" required/>
-                    <span id='nameField-error'></span>
-                    <input id="titleField" className="popup__field popup__field_title" type="text" name="about"
-                           placeholder="О себе"
-                           minLength="2" maxLength="200" required/>
-                    <span id='titleField-error'></span>
-                </PopupWithForm>
-                <PopupWithForm id="popupAdd" title='Новое место' name='add-card' isOpen={isAddPlacePopupOpen}
-                               onClose={closeAllPopups}>
-                    <input id="placeField" className="popup__field popup__field_add-card popup__field_place" type="text"
-                           name="name"
-                           placeholder="Название" minLength="2" maxLength="30" required/>
-                    <span id='placeField-error'></span>
-                    <input id="sourceField" className="popup__field popup__field_add-card popup__field_source"
-                           type="url"
-                           name="link"
-                           placeholder="Ссылка на картинку" required/>
-                    <span id='sourceField-error'></span>
-                </PopupWithForm>
-                <PopupWithForm id='popupAvatar' title='Обновить аватар' name='update-avatar'
-                               isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
-                    <input id="updateField" className="popup__field popup__field_update" type="url" name="link"
-                           placeholder="Ссылка на картинку" required/>
-                    <span id='updateField-error'></span>
-                </PopupWithForm>
-                <PopupWithForm id='popupConfirm' title='Вы уверены?' name='confirm'>
-                </PopupWithForm>
-                <ImagePopup id='popupImage' card={selectedCard} onClose={closeAllPopups}>
-                </ImagePopup>
+                    <PopupWithForm id='popupEdit' title='Редактировать профиль' name='edit-profile'
+                                   isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
+                        <input id="nameField" className="popup__field popup__field_name" type="text" name="name"
+                               placeholder="Имя"
+                               minLength="2" maxLength="40" required/>
+                        <span id='nameField-error'></span>
+                        <input id="titleField" className="popup__field popup__field_title" type="text" name="about"
+                               placeholder="О себе"
+                               minLength="2" maxLength="200" required/>
+                        <span id='titleField-error'></span>
+                    </PopupWithForm>
+                    <PopupWithForm id="popupAdd" title='Новое место' name='add-card' isOpen={isAddPlacePopupOpen}
+                                   onClose={closeAllPopups}>
+                        <input id="placeField" className="popup__field popup__field_add-card popup__field_place" type="text"
+                               name="name"
+                               placeholder="Название" minLength="2" maxLength="30" required/>
+                        <span id='placeField-error'></span>
+                        <input id="sourceField" className="popup__field popup__field_add-card popup__field_source"
+                               type="url"
+                               name="link"
+                               placeholder="Ссылка на картинку" required/>
+                        <span id='sourceField-error'></span>
+                    </PopupWithForm>
+                    <PopupWithForm id='popupAvatar' title='Обновить аватар' name='update-avatar'
+                                   isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
+                        <input id="updateField" className="popup__field popup__field_update" type="url" name="link"
+                               placeholder="Ссылка на картинку" required/>
+                        <span id='updateField-error'></span>
+                    </PopupWithForm>
+                    <PopupWithForm id='popupConfirm' title='Вы уверены?' name='confirm'>
+                    </PopupWithForm>
+                    <ImagePopup id='popupImage' card={selectedCard} onClose={closeAllPopups}>
+                    </ImagePopup>
+                </div>
             </div>
-        </div>
+        </UserContext.Provider>
+
     );
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, {useContext}from "react";
+import {UserContext} from "../contexts/CurrentUserContext";
 
-function Card(props) {
-    function handleClick() {
-        props.onCardClick(props.item);
-    }
+function Card({item}) {
+    const currentUser = useContext(UserContext);
+    // function handleClick() {
+    //     props.onCardClick(props.item);
+    // }
+
+    // Определяем, являемся ли мы владельцем текущей карточки
+    const isOwn = item.owner._id === currentUser._id;
+
+// Создаём переменную, которую после зададим в `className` для кнопки удаления
+    const cardDeleteButtonClassName = (
+        `element__button-trash ${isOwn ? 'element__button-trash_visible' : ''}`
+    );
 
     return (
         <li className="element">
-            <img className="element__image" src={props.item.link} alt="#" onClick={handleClick}/>
-            <button className="element__button-trash" type="button" />
+            <img className="element__image" src={item.link} alt="#" />
+            <button className={cardDeleteButtonClassName} type="button" />
             <div className="element__name-place">
-                <h2 className="element__name-title">{props.item.name}</h2>
+                <h2 className="element__name-title">{item.name}</h2>
                 <div className="element__like">
                     <button id="likeButton" className="element__button-like" type="button" />
-                    <p className="element__like-counter">{props.item.likes.length}</p>
+                    <p className="element__like-counter">{item.likes.length}</p>
                 </div>
 
 
